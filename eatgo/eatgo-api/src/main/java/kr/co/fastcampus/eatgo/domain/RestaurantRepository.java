47421cba80dd5e1e697bbcd2867cea9df70894b2
sagekhw <sagekhw@gmail.com>
2020-08-12 19:55:37 +0900

    의존성 주입

--- eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/RestaurantRepository.java
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/RestaurantRepository.java
@@ -1,28 +1,9 @@
 package kr.co.fastcampus.eatgo.domain;
 
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class RestaurantRepository {
+public interface RestaurantRepository {
+    List<Restaurant> findAll();
 
-    private List<Restaurant> restaurants = new ArrayList<>();
-
-    public RestaurantRepository(){
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food","Seoul"));
-    }
-
-    public List<Restaurant> findAll() {
-        return restaurants;
-    }
-
-    public Restaurant findById(long id) {
-        return restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
+    Restaurant findById(long id);
 }
--- eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,6 +1,7 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,11 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(RestaurantController.class)
@@ -25,7 +23,7 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean // 의존성을 직접 연결해줘야 하므로 Spring Boot에는 @SpyBean을 사용
+    @SpyBean(RestaurantRepositoryImpl.class)
     private RestaurantRepository restaurantRepository;
 
     @Test
@@ -39,7 +37,6 @@ public class RestaurantControllerTest {
 
     @Test
     public void detail() throws Exception{
-
         mvc.perform(get("/restaurants/2020"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":2020")))
